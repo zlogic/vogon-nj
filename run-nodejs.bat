c74c708ba5225457e32d036d8e6a75f0e564df46
Dmitry Zolotukhin <zlogic@gmail.com>
2016-02-22 11:33:25 +0300

    Added Windows launch scripts. Fixed typo in comment.

---
+++ run-nodejs.bat
@@ -0,0 +1,4 @@
+call "c:\Program Files\nodejs\nodevars.bat"
+SET DEBUG=vogon-nj:*
+SET ALLOW_REGISTRATION=true
+npm run-script nodemon
---
+++ run-tests.bat
@@ -0,0 +1,2 @@
+call "c:\Program Files\nodejs\nodevars.bat"
+npm run-script test
