99b3cb6398fed160a29cf5ddb1dccd994cc0bacf
Dmitry Zolotukhin <zlogic@gmail.com>
2016-03-05 17:25:58 +0300

    Added tests for getting user data and list of user accounts. Added helper functions to perform OAuth authentication. Added a user to DB prepopulation script.

--- test/prepopulate.js
+++ test/prepopulate.js
@@ -1,7 +1,7 @@
 var dbService = require('../services/model');
 
 var prepopulate = function(){
-  var user;
+  var users;
   return dbService.sequelize.transaction(function(transaction){
     return dbService.User.create({
       username: "user01",
@@ -27,11 +27,42 @@ var prepopulate = function(){
           ]
         }
       ]
-    }, {include: [dbService.Account, {model: dbService.FinanceTransaction, include: [dbService.FinanceTransactionComponent]}], transaction: transaction}).then(function(createdUser){
-      user = createdUser;
-      return user.FinanceTransactions[0].FinanceTransactionComponents[0].setAccount(user.Accounts[0], {transaction: transaction});
+    }, {include: [dbService.Account, {model: dbService.FinanceTransaction, include: [dbService.FinanceTransactionComponent]}], transaction: transaction}).then(function(user){
+      return dbService.sequelize.Promise.all([
+        user.FinanceTransactions[0].FinanceTransactionComponents[0].setAccount(user.Accounts[0], {transaction: transaction})
+      ]);
+    }).then(function(){
+      return dbService.User.create({
+        username: "user02",
+        password: "mypassword2",
+        Accounts: [
+          {
+            name: "test account 3",
+            balance: 5,
+            currency: "RUB",
+            includeInTotal: true,
+            showInList: true
+          }
+        ],
+        FinanceTransactions: [
+          {
+            description: "test transaction 3",
+            type: "expenseincome",
+            date: "2014-05-17",
+            tags: ["hello", "world"],
+            amount: 3,
+            FinanceTransactionComponents: [
+              {amount: 100}
+            ]
+          }
+        ]
+      }, {include: [dbService.Account, {model: dbService.FinanceTransaction, include: [dbService.FinanceTransactionComponent]}], transaction: transaction}).then(function(user){
+        return dbService.sequelize.Promise.all([
+          user.FinanceTransactions[0].FinanceTransactionComponents[0].setAccount(user.Accounts[0], {transaction: transaction})
+        ]);
+      });
     });
   });
-}
+};
 
 module.exports = prepopulate;
--- test/service.js
+++ test/service.js
@@ -14,6 +14,22 @@ var morgan = require('morgan');
 var port = 3000;
 var baseUrl = "http://localhost:" + port;
 
+var authenticateUser = function(userData, callback){
+  superagent.post(baseUrl + "/oauth/token")
+    .send("username=" + userData.username)
+    .send("password=" + userData.password)
+    .send("grant_type=password")
+    .send("client_id=vogonweb")
+    .end(function(err, result){
+      if(err) return callback(err);
+      callback(null, result.body.access_token, result);
+    });
+};
+
+var tokenHeader = function(token){
+  return {Authorization: "Bearer " + token};
+};
+
 describe('Service', function() {
   var server;
   before(function(){
@@ -48,7 +64,7 @@ describe('Service', function() {
     }
   );
 
-  describe('actions', function () {
+  describe('registration', function () {
     it('should be able to register a new user if registration is allowed', function (done) {
       var userData = {username: "user01", password: "password"};
       process.env.ALLOW_REGISTRATION = true;
@@ -71,7 +87,6 @@ describe('Service', function() {
         } catch(err) {done(err);}
       });
     });
-
     it('should not be able to register a new user if registration is not allowed', function (done) {
       var userData = {username: "user01", password: "password"};
       process.env.ALLOW_REGISTRATION = false;
@@ -97,16 +112,174 @@ describe('Service', function() {
             assert.equal(result.status, 500);
             assert.deepEqual(result.body, {exception : i18n.__('User already exists')});
             dbService.User.findAll().then(function(users){
-              assert.equal(users.length, 1);
+              assert.equal(users.length, 2);
               assert.equal(users[0].username, 'user01');
+              assert.equal(users[1].username, 'user02');
               users[0].validatePassword('mypassword', function(err, passwordValid){
                 if(err) return done(err);
                 try {
                   assert.equal(passwordValid, true);
-                  done();
+                  users[1].validatePassword('mypassword2', function(err, passwordValid){
+                    if(err) return done(err);
+                    try {
+                      assert.equal(passwordValid, true);
+                      done();
+                    } catch (err) { done(err) };
+                  });
                 } catch (err) { done(err) };
-              })
+              });
             }).catch(done)
+          } catch(err) {done(err);}
+        });
+      }).catch(done);
+    });
+    it('should accept authentication of a valid user', function (done) {
+      var userData = {username: "user01", password: "mypassword"};
+      prepopulate().then(function(){
+        authenticateUser(userData, function(err, token, result){
+          if(err) return done(err);
+          try {
+            assert.equal(result.status, 200);
+            assert.ok(token);
+            done();
+          } catch(err) {done(err);}
+        });
+      }).catch(done);
+    });
+    it('should reject authentication of an invalid user', function (done) {
+      var userData = {username: "user01", password: "badpassword"};
+      prepopulate().then(function(){
+        authenticateUser(userData, function(err, token, result){
+          try {
+            assert.ok(err);
+            assert.equal(err.status, 500);
+            assert.equal(err.response.body.error_description, i18n.__('Bad credentials'));
+            assert.equal(!!token, false);
+            done();
+          } catch(err) {done(err);}
+        });
+      }).catch(done);
+    });
+  });
+
+  describe('userdata', function () {
+    it('should get details for an authenticated user', function (done) {
+      var userData = {username: "user01", password: "mypassword"};
+      prepopulate().then(function(){
+        authenticateUser(userData, function(err, token, result){
+          if(err) return done(err);
+          superagent.get(baseUrl + "/service/user").set(tokenHeader(token)).end(function(err, result){
+            if(err) return done(err);
+            try {
+              assert.ok(result);
+              assert.equal(result.status, 200);
+              assert.deepEqual(result.body, { username: 'user01', version: 0 });
+              done();
+            } catch(err) {done(err);}
+          });
+        });
+      }).catch(done);
+    });
+    it('should ignore id in requests for getting user data and use OAuth data instead', function (done) {
+      var userData = {username: "user01", password: "mypassword"};
+      prepopulate().then(function(){
+        authenticateUser(userData, function(err, token, result){
+          if(err) return done(err);
+          superagent.get(baseUrl + "/service/user").set(tokenHeader(token)).send({id: 2}).end(function(err, result){
+            if(err) return done(err);
+            try {
+              assert.ok(result);
+              assert.equal(result.status, 200);
+              assert.deepEqual(result.body, { username: 'user01', version: 0 });
+              done();
+            } catch(err) {done(err);}
+          });
+        });
+      });
+    });
+    it('should not be able to get user data for an unauthenticated user (no token)' , function (done) {
+      prepopulate().then(function(){
+        superagent.get(baseUrl + "/service/user").end(function(err, result){
+          try {
+            assert.ok(err);
+            assert.equal(err.status, 401);
+            assert.equal(err.response.text, 'Unauthorized');
+            done();
+          } catch(err) {done(err);}
+        });
+      }).catch(done);
+    });
+    it('should not be able to get user data for an unauthenticated user (bad token)', function (done) {
+      prepopulate().then(function(){
+        var token = 'aaaa';
+        superagent.get(baseUrl + "/service/user").set(tokenHeader(token)).end(function(err, result){
+          try {
+            assert.ok(err);
+            assert.equal(err.status, 401);
+            assert.equal(err.response.text, 'Unauthorized');
+            done();
+          } catch(err) {done(err);}
+        });
+      }).catch(done);
+    });
+  });
+
+  describe('accounts', function () {
+    it('should get a list of accounts for an authenticated user', function (done) {
+      var userData = {username: "user01", password: "mypassword"};
+      prepopulate().then(function(){
+        authenticateUser(userData, function(err, token, result){
+          if(err) return done(err);
+          superagent.get(baseUrl + "/service/accounts").set(tokenHeader(token)).end(function(err, result){
+            if(err) return done(err);
+            try {
+              assert.ok(result);
+              assert.equal(result.status, 200);
+              assert.deepEqual(result.body, [{ balance: 100, id: 1, name: 'test account 1', currency: 'RUB', includeInTotal: true, showInList: true, version: 0 }]);
+              done();
+            } catch(err) {done(err);}
+          });
+        });
+      }).catch(done);
+    });
+    it('should ignore UserId in requests for getting a list of accounts and use OAuth data instead', function (done) {
+      var userData = {username: "user01", password: "mypassword"};
+      prepopulate().then(function(){
+        authenticateUser(userData, function(err, token, result){
+          if(err) return done(err);
+          superagent.get(baseUrl + "/service/accounts").set(tokenHeader(token)).send({UserId: 2}).end(function(err, result){
+            if(err) return done(err);
+            try {
+              assert.ok(result);
+              assert.equal(result.status, 200);
+              assert.deepEqual(result.body, [{ balance: 100, id: 1, name: 'test account 1', currency: 'RUB', includeInTotal: true, showInList: true, version: 0 }]);
+              done();
+            } catch(err) {done(err);}
+          });
+        });
+      });
+    });
+    it('should not be able to get a list of accounts for an unauthenticated user (no token)' , function (done) {
+      prepopulate().then(function(){
+        superagent.get(baseUrl + "/service/accounts").end(function(err, result){
+          try {
+            assert.ok(err);
+            assert.equal(err.status, 401);
+            assert.equal(err.response.text, 'Unauthorized');
+            done();
+          } catch(err) {done(err);}
+        });
+      }).catch(done);
+    });
+    it('should not be able to get a list of accounts for an unauthenticated user (bad token)', function (done) {
+      prepopulate().then(function(){
+        var token = 'aaaa';
+        superagent.get(baseUrl + "/service/accounts").set(tokenHeader(token)).end(function(err, result){
+          try {
+            assert.ok(err);
+            assert.equal(err.status, 401);
+            assert.equal(err.response.text, 'Unauthorized');
+            done();
           } catch(err) {done(err);}
         });
       }).catch(done);
