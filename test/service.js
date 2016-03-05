dbf07bc9e7d7d482a814b13ab50474b11e877f4f
Dmitry Zolotukhin <zlogic@gmail.com>
2016-03-05 20:44:00 +0300

    Added currency service tests.

--- test/service.js
+++ test/service.js
@@ -793,4 +793,51 @@ describe('Service', function() {
       }).catch(done);
     });
   });
+
+  describe('currencies', function () {
+    it('should get a list of all possible currencies for an authenticated user', function (done) {
+      var userData = {username: "user01", password: "mypassword"};
+      prepopulate().then(function(){
+        authenticateUser(userData, function(err, token, result){
+          if(err) return done(err);
+          superagent.get(baseUrl + "/service/currencies").set(tokenHeader(token)).end(function(err, result){
+            if(err) return done(err);
+            try {
+              assert.ok(result);
+              assert.equal(result.status, 200);
+              assert.deepEqual(result.body.filter(function(currency){return currency.currencyCode === "RUB";}), [{currencyCode:"RUB", displayName: "Russian rouble"}]);
+              assert.deepEqual(result.body.filter(function(currency){return currency.currencyCode === "EUR";}), [{currencyCode:"EUR", displayName: "Euro"}]);
+              assert.deepEqual(result.body.filter(function(currency){return currency.currencyCode === "USD";}), [{currencyCode:"USD", displayName: "United States dollar"}]);
+              done();
+            } catch(err) {done(err);}
+          });
+        });
+      }).catch(done);
+    });
+    it('should not be able to get a list of all possible currencies for an unauthenticated user (no token)' , function (done) {
+      prepopulate().then(function(){
+        superagent.get(baseUrl + "/service/currencies").end(function(err, result){
+          try {
+            assert.ok(err);
+            assert.equal(err.status, 401);
+            assert.equal(err.response.text, 'Unauthorized');
+            done();
+          } catch(err) {done(err);}
+        });
+      }).catch(done);
+    });
+    it('should not be able to get a list of all possible currencies for an unauthenticated user (bad token)', function (done) {
+      prepopulate().then(function(){
+        var token = 'aaaa';
+        superagent.get(baseUrl + "/service/currencies").set(tokenHeader(token)).end(function(err, result){
+          try {
+            assert.ok(err);
+            assert.equal(err.status, 401);
+            assert.equal(err.response.text, 'Unauthorized');
+            done();
+          } catch(err) {done(err);}
+        });
+      }).catch(done);
+    });
+  });
 });
