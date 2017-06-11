d7591a28123c1182abf06aff592591c07c26ef81
Dmitry Zolotukhin <zlogic@gmail.com>
2017-06-11 17:07:57 +0200

    Added login/register modes to login form.

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 import { VogonMaterialModule } from './material.module';
 
@@ -20,7 +21,8 @@ import { UsersettingsComponent } from './components/usersettings.component';
     BrowserModule,
     BrowserAnimationsModule,
     VogonRoutingModule,
-    VogonMaterialModule
+    VogonMaterialModule,
+    FormsModule
   ],
   declarations: [
     AppComponent,
--- app/components/login.component.ts
+++ app/components/login.component.ts
@@ -1,7 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  templateUrl: '../templates/components/login.pug'
+  templateUrl: '../templates/components/login.pug',
 })
 
-export class LoginComponent { }
+export class LoginComponent {
+  mode: string = "login";
+
+  modeIsLogin(): boolean { return this.mode == "login"; }
+  modeIsRegister(): boolean { return this.mode == "register"; }
+}
