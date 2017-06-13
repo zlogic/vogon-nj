3a59c72e70b009ee7064f46388ad3f7102e0c3bd
Dmitry Zolotukhin <zlogic@gmail.com>
2017-06-13 19:08:54 +0200

    Fixed Sequelize logger for tests.

--- test/utils/dbconfiguration.js
+++ test/utils/dbconfiguration.js
@@ -3,12 +3,12 @@ var dbService = require('../../services/dbservice');
 var path = require('path');
 var testdir = require('./testdir');
 var Sequelize = require('sequelize');
-var logger = require('../../services/logger').logger;
+var logger = require('../../services/logger');
 
 var reconfigureDb = function(inMemory){
   inMemory = inMemory !== undefined ? inMemory : (process.env.DOCKER_BUILD || true);
   var storage = inMemory === true ? ":memory:" : path.resolve(testdir.tmpdir, "vogon-nj.sqlite");
-  var currentDbService = model.model("sqlite:", {storage: storage, isolationLevel: Sequelize.Transaction.ISOLATION_LEVELS.SERIALIZABLE, logging: logger.verbose});
+  var currentDbService = model.model("sqlite:", {storage: storage, isolationLevel: Sequelize.Transaction.ISOLATION_LEVELS.SERIALIZABLE, logging: logger.sequelizeLogger});
   var promise = dbService.sequelize.close();
   for(var k in currentDbService)
     dbService[k] = currentDbService[k];
